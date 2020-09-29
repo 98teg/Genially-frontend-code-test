254891fbfcfd67bdb3878b0a2456b056aafcf0ba
98teg <teg98tom@gmail.com>
2020-09-29 16:59:05 +0200

    Create box when clicking the button

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -1,14 +1,37 @@
 import React from "react";
 
-function Toolbar() {
-  return (
-    <div className="toolbar">
-      <button>Add Box</button>
-      <button>Remove Box</button>
-      <input type="color" />
-      <span>No boxes selected</span>
-    </div>
-  );
+import uuid from "uuid/v4";
+import BoxModel from "../stores/models/Box";
+import getRandomColor from "../utils/getRandomColor";
+
+class Toolbar extends React.Component {
+  constructor () {
+    super()
+  
+    this.addBox = this.addBox.bind(this)
+  }
+
+  addBox () {
+    const box = BoxModel.create({
+      id: uuid(),
+      color: getRandomColor(),
+      left: 0,
+      top: 0
+    });
+    
+    this.props.store.addBox(box);
+  }
+
+  render () {
+    return (
+      <div className="toolbar">
+        <button onClick={this.addBox}>Add Box</button>
+        <button>Remove Box</button>
+        <input type="color" />
+        <span>No boxes selected</span>
+      </div>
+    );
+  }
 }
 
 export default Toolbar;
