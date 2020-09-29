26b20abf99dfde307ec9ab6c69ffb314883a1307
98teg <teg98tom@gmail.com>
2020-09-29 16:58:35 +0200

    Create new views

--- src/stores/MainStore.js
+++ src/stores/MainStore.js
@@ -1,30 +1,26 @@
 import { types } from "mobx-state-tree";
-import uuid from "uuid/v4";
 import BoxModel from "./models/Box";
-import getRandomColor from "../utils/getRandomColor";
 
 const MainStore = types
   .model("MainStore", {
     boxes: types.array(BoxModel)
   })
+  .views(self => ({
+    getBox(id) {
+      return self.boxes.find(box => box.id === id)
+    },
+    getSelectedBoxes() {
+      return self.boxes.filter(box => box.selected)
+    }
+  }))
   .actions(self => {
     return {
       addBox(box) {
         self.boxes.push(box);
       }
     };
-  })
-  .views(self => ({}));
+  });
 
 const store = MainStore.create();
-
-const box1 = BoxModel.create({
-  id: uuid(),
-  color: getRandomColor(),
-  left: 0,
-  top: 0
-});
-
-store.addBox(box1);
 
 export default store;
